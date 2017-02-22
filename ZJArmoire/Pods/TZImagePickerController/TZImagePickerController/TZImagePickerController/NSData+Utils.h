5eac35bff761b5aaf595a39640e13dc279249f01
iBlock <wsiBlock@gmail.com>
2017-02-22 11:22:16 +0800

    提交文件

---
+++ ZJArmoire/Pods/TZImagePickerController/TZImagePickerController/TZImagePickerController/NSData+Utils.h
@@ -0,0 +1,15 @@
+//
+//  NSData+Utils.h
+//  TZImagePickerController
+//
+//  Created by iBlock on 2017/2/21.
+//  Copyright © 2017年 谭真. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (Utils)
+
+- (NSString *)MD5;
+
+@end
